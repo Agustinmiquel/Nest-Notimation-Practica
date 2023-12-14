43f749ebb359e6935dc3b553e1815cb06648b42d
Agustinmiquel <agumiquel@.com>
2023-12-14 00:18:35 -0300

    añadiendo el .ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { DivisionesModule } from './divisiones/divisiones.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ClubesModule,
     DivisionesModule,
     UsuariosModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://agusm:root@cluster0.xlthkyt.mongodb.net/Notimotion?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_UR),
     AuthModule,
   ],
   controllers: [],
