6e9565882b0908dbf1aa67dd3388eea3b1f9c0e6
Agustinmiquel <agumiquel@.com>
2023-12-26 12:16:16 -0300

    .

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -7,7 +7,6 @@ import {
 
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { jwtConstants } from 'src/app/shared/constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -22,7 +21,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: jwtConstants.secret,
+        secret: process.env.JWTSECRET,
       });
       request['user'] = payload;
     } catch (error) {
